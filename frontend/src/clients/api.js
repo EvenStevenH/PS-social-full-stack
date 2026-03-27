04cfb2cba34344eafd6dfbb9ece78928b0e615e7
Steven Huang <22431150+EvenStevenH@users.noreply.github.com>
2026-03-26 21:48:53 -0400

    update gitignore

--- frontend/src/clients/api.js
+++ frontend/src/clients/api.js
@@ -1,16 +1,20 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+
 export const token = () => localStorage.getItem("token");
 
 export const userClient = axios.create({
-	baseURL: "http://localhost:3002/api/users",
+	// baseURL: "http://localhost:3002/api/users",
+	baseURL: BASE_URL + "/api/users",
 	headers: {
 		Authorization: `Bearer ${token()}`,
 	},
 });
 
 export const postClient = axios.create({
-	baseURL: "http://localhost:3002/api/posts",
+	// baseURL: "http://localhost:3002/api/posts",
+	baseURL: `${BASE_URL}/api/posts`,
 	// IF not using interceptors
 	// headers: {
 	// 	Authorization: `Bearer ${token}`,
